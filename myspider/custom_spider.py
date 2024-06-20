7e949eda35bdb2a6de7f65e863bb6ffe5ea1ab27
qianyue996 <3010875645@qq.com>
2024-06-20 20:11:12 +0800

    ''

---
+++ myspider/custom_spider.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+from playwright.sync_api import sync_playwright
+
+
+
+
+if __name__ == "__main__":
+    chrome_port = 9999
+
+    # open the local Chrome
+    chrome_path = r'"C:\Program Files\Google\Chrome\Application\chrome.exe"'
+    debugging_port = f"--remote-debugging-port={chrome_port}"
+    command = f"{chrome_path} {debugging_port}"
+
+    subprocess.Popen(command, shell=True)
+    # or
+    # os.system(command)
+
+    with sync_playwright() as p:
+        browser = p.chromium.connect_over_cdp(f"http://localhost:{chrome_port}")
+        context = browser.contexts[0]
+        page = context.new_page()
+        pass
--- myspider/run.py
+++ myspider/run.py
@@ -2,6 +2,9 @@ import os
 
 from scrapy import cmdline
 
+"""
+使用chrome的debug模式，直接控制本地chrome浏览器，达到模拟用户行为，真正做到了反爬(playwright)
+"""
 # spider_path = os.path.abspath(__file__)
 # 上次运行到网络工程师 page9
 clash_path = os.path.abspath(r'..\clash-agent')
