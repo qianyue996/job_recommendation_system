7240e27558f87846f6712dd695816502b4a42a72
qianyue996 <3010875645@qq.com>
2024-06-22 10:20:43 +0800

    modify

--- myspider/custom_spider.py
+++ myspider/custom_spider.py
@@ -1,10 +1,14 @@
 import os
 import subprocess
 
+from lxml import etree
 from playwright.sync_api import sync_playwright
 
 
 if __name__ == "__main__":
+    """
+    打开浏览器，在浏览器目录下打开命令行 '.\chrome.exe --remote-debugging-port=9999'
+    """
     chrome_port = 9999
 
     # # open the local Chrome
@@ -17,4 +21,16 @@ if __name__ == "__main__":
         browser = p.chromium.connect_over_cdp(f"http://localhost:{chrome_port}")
         context = browser.contexts[0]
         page = context.new_page()
+        page.goto('https://www.zhipin.com')
+        res = page.content()
+
+        """
+        爬取职位类型共25个，下属的职业的超链接以及名称，用于制作起始url
+        """
+        tree = etree.HTML(res)
+        # 25个职业类型文本
+        job_type_list = tree.xpath("//div[@class='home-job-menu']/div[1]/dl/dd/b/text()")
+        # 因为最后一个职业类型是其他，直接舍去，故为24个职业类型
+        for job in len(job_type_list):
+            tree.xpath(f"//div[@class='home-job-menu']/div[1]/dl[{job+1}]")
         pass
