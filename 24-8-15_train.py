1a2e222966c55706a32783512c8d63b4b239a4ac
qianyue996 <3010875645@qq.com>
2024-09-01 16:38:02 +0800

    change

--- 24-8-15_train.py
+++ 24-8-15_train.py
@@ -1,3 +1,5 @@
+import os
+
 import tqdm
 import torch
 import pandas as pd
@@ -59,7 +61,76 @@ class MyDataset(torch.utils.data.Dataset):
         }
         return inputs, labels
 
-if __name__ == '__main__':
+def train(model, train_dataloader, optimizer, epoch):
+    model.train()
+    old_loss = 999999
+    bar1 = tqdm.tqdm(enumerate(train_dataloader), desc='Progress', unit='step', total=len(train_dataloader))
+    for i, data in bar1:
+        # 清空优化器的梯度
+        optimizer.zero_grad()
+        # 从批次中提取输入数据和标签
+        input, labels = data
+        input_ids = input['input_ids'].to('cuda')
+        attention_mask = input['attention_mask'].to('cuda')
+        outputs = model(input_ids, attention_mask)
+        label1 = labels['label_level1'].to('cuda')
+        label2 = labels['label_level2'].to('cuda')
+        # 计算损失
+        loss = loss_fn.forward(outputs, label1, label2)
+        # 反向传播损失
+        loss.backward()
+        # 更新模型参数
+        optimizer.step()
+        # 取此批次的最小loss
+        if loss <= old_loss:
+            old_loss = loss
+        bar1.set_postfix({
+            'epoch' : f'{epoch}',
+            'loss' : f'{old_loss.item():.3f}',
+            'lr' : optimizer.state_dict()['param_groups'][0]['lr']
+            })
+    return model
+
+def val(model, val_dataloader, epoch):
+    # 验证阶段
+    model.eval()
+    # 评估参数
+    num_examples = 0
+    total_correct = 0
+    total_loss = 0.
+    with torch.no_grad():
+        val_bar = tqdm.tqdm(enumerate(val_dataloader), desc='Progress', unit='step', total=len(val_dataloader))
+        for i, data in val_bar:
+            input, labels = data
+            input_ids = input['input_ids'].to('cuda')
+            attention_mask = input['attention_mask'].to('cuda')
+            label1 = labels['label_level1'].to('cuda')
+            label2 = labels['label_level2'].to('cuda')
+            preds = model(input_ids, attention_mask)
+
+            num_examples += len(input_ids)
+            for i in range(len(preds)):
+                level1_correct = (preds[0].argmax(dim=-1) == label1.argmax(dim=-1)).sum().item()
+                if level1_correct == 0:
+                    correct = 0
+                else:
+                    level2_correct = (preds[1].argmax(dim=-1) == label2.argmax(dim=-1)).sum().item()
+                    correct = level1_correct + level2_correct
+            total_correct += correct
+            accuracy = total_correct / num_examples
+            total_loss += loss.item()
+            avg_loss = total_loss / num_examples
+            val_bar.set_postfix(epoch=epoch, accuracy=accuracy, avg_loss=avg_loss)
+    return accuracy
+
+def model_save(model, accuracy, epoch):
+    if not os.path.exists("checkpoints"):
+        os.mkdir("checkpoints")
+    model_name = f'epoch={epoch}_accuracy={accuracy}.pth'
+    save_dir = os.path.join("checkpoints", model_name)
+    torch.save(model, save_dir)
+
+def main():
     train_file = 'train.xlsx'
     label_file = 'data/_label.json'
     dataset = MyDataset(train_file)
@@ -72,61 +143,16 @@ if __name__ == '__main__':
 
     num1, num2 = dataset.class_num()
     model = MyModel(len(num1), len(num2)).to('cuda')
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
-    epochs = 20
-    model.train()
-    for epoch in range(epochs):
-        old_loss = 999999
-        bar1 = tqdm.tqdm(enumerate(train_dataloader), desc='Progress', unit='step', total=len(train_dataloader))
-        for i, data in bar1:
-            # 清空优化器的梯度
-            optimizer.zero_grad()
-            # 从批次中提取输入数据和标签
-            input, labels = data
-            input_ids = input['input_ids'].to('cuda')
-            attention_mask = input['attention_mask'].to('cuda')
-            outputs = model(input_ids, attention_mask)
-            label1 = labels['label_level1'].to('cuda')
-            label2 = labels['label_level2'].to('cuda')
-            # 计算损失
-            loss = loss_fn.forward(outputs, label1, label2)
-            # 反向传播损失
-            loss.backward()
-            # 更新模型参数
-            optimizer.step()
-            # 取此批次的最小loss
-            if loss <= old_loss:
-                old_loss = loss
-            bar1.set_postfix({
-                'epoch' : f'{epoch}',
-                'loss' : f'{old_loss.item():.3f}',
-                })
-        # 验证阶段
-        model.eval()
-        # 评估参数
-        num_examples = 0
-        total_correct = 0
-        total_loss = 0.
-        with torch.no_grad():
-            val_bar = tqdm.tqdm(enumerate(val_dataloader), desc='Progress', unit='step', total=len(val_dataloader))
-            for i, data in val_bar:
-                input, labels = data
-                input_ids = input['input_ids'].to('cuda')
-                attention_mask = input['attention_mask'].to('cuda')
-                label1 = labels['label_level1'].to('cuda')
-                label2 = labels['label_level2'].to('cuda')
-                preds = model(input_ids, attention_mask)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+    optimizer = torch.optim.AdamW(params=model.parameters(), lr=1e-5, weight_decay=0.01)
 
-                num_examples += len(input_ids)
-                for i in range(len(preds)):
-                    level1_correct = (preds[0].argmax(dim=-1) == label1.argmax(dim=-1)).sum().item()
-                    if level1_correct == 0:
-                        correct = 0
-                    else:
-                        level2_correct = (preds[1].argmax(dim=-1) == label2.argmax(dim=-1)).sum().item()
-                        correct = level1_correct + level2_correct
-                total_correct += correct
-                accuracy = total_correct / num_examples
-                total_loss += loss.item()
-                avg_loss = total_loss / num_examples
-                val_bar.set_postfix(epoch=epoch, accuracy=accuracy, avg_loss=avg_loss)
+    epochs = 20
+    for epoch in range(epochs):
+        model = train(model, train_dataloader, optimizer)
+        accuracy = val(model, val_dataloader)
+        if accuracy > 0.7:
+            model_save(model, accuracy, epoch)
+
+
+if __name__ == '__main__':
+    main()
